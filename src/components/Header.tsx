d8807280c1d7e57c2e3ff4bcaf9628129d1fba7b
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-31 20:32:54 -0300

    feat: :lipstick: Componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.css";
+
+import { ReactComponent as DogIcon } from "../assets/dog-icon.svg";
+
 export function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <DogIcon />
+        </Link>
+
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
 }
