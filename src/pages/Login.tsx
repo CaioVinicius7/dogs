9ca1b6181238518ac93ba9a36bf6fc8ab5bc02d7
Caio Vinícius <caio1525pereira@gmail.com>
2023-06-01 18:24:20 -0300

    feat: :construction: Estrutura inicial da página Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,41 @@
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
 export function Login() {
-  return <h1>Login</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log({
+      username,
+      password
+    });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </form>
+
+      <Link to="/login/create">Cadastro</Link>
+    </section>
+  );
 }
