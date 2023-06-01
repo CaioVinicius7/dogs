8609dbab448c148f6725cbbddc5f37651f27f3e2
Caio Vinícius <caio1525pereira@gmail.com>
2023-06-01 18:06:13 -0300

    feat: :wrench: Rotas de login

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { LoginLayout } from "./layouts/LoginLayout";
 
 export function Router() {
   return (
     <Routes>
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+
+        <Route path="/login/*" element={<LoginLayout />}>
+          <Route index element={<Login />} />
+          <Route path="create" element={<h1>/login/create</h1>} />
+          <Route path="password/lost" element={<h1>/login/password/lost</h1>} />
+          <Route
+            path="password/reset"
+            element={<h1>/login/password/reset</h1>}
+          />
+        </Route>
       </Route>
     </Routes>
   );
