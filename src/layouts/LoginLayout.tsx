cb7c46f5b37ca789b2c02f3130d59fb85706eb0d
Caio Vinícius <caio1525pereira@gmail.com>
2023-06-04 20:28:18 -0300

    feat: :children_crossing: Navegação caso usuário esteja logado

--- src/layouts/LoginLayout.tsx
+++ src/layouts/LoginLayout.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+
+import { useAuthContext } from "../contexts/AuthContext";
 
 export function LoginLayout() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuthContext();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/account");
+    }
+  }, [navigate, isAuthenticated]);
+
   return (
     <div className="container">
       <Outlet />
