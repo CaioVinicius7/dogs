77e53cbabedbcf2285e8a97daf187d80eb363876
Caio Vinícius <caio1525pereira@gmail.com>
2023-06-22 21:44:09 -0300

    feat: :lipstick: Botão do menu mobile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 
 import styles from "./Navigation.module.css";
 
+import { useMedia } from "../hooks/useMedia";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 import { ReactComponent as Feed } from "../assets/feed.svg";
@@ -11,29 +12,48 @@ import { ReactComponent as Plus } from "../assets/plus.svg";
 import { ReactComponent as Logout } from "../assets/logout.svg";
 
 export function Navigation() {
-  const [isMobile] = useState(null);
-
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   const { logout } = useAuthContext();
 
-  return (
-    <nav className={styles.nav}>
-      <NavLink to="/account" end>
-        <Feed />
-        {isMobile && "Minhas Fotos"}
-      </NavLink>
-      <NavLink to="/account/stats">
-        <Stats />
-        {isMobile && "Estatísticas"}
-      </NavLink>
-      <NavLink to="/account/post">
-        <Plus />
-        {isMobile && "Adicionar Foto"}
-      </NavLink>
+  const isMobile = useMedia("(max-width: 40rem)"); // 40rem = 540px
 
-      <button onClick={logout}>
-        <Logout />
-        {isMobile && "Sair"}
-      </button>
-    </nav>
+  function handleChangeMenuVisibility() {
+    setMenuIsOpen((state) => !state);
+  }
+
+  return (
+    <>
+      {isMobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            menuIsOpen && styles.mobileButtonActive
+          }`}
+          onClick={handleChangeMenuVisibility}
+        />
+      )}
+
+      {!isMobile && (
+        <nav className={styles.nav}>
+          <NavLink to="/account" end>
+            <Feed />
+            {isMobile && "Minhas Fotos"}
+          </NavLink>
+          <NavLink to="/account/stats">
+            <Stats />
+            {isMobile && "Estatísticas"}
+          </NavLink>
+          <NavLink to="/account/post">
+            <Plus />
+            {isMobile && "Adicionar Foto"}
+          </NavLink>
+
+          <button onClick={logout}>
+            <Logout />
+            {isMobile && "Sair"}
+          </button>
+        </nav>
+      )}
+    </>
   );
 }
