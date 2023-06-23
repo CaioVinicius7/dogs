8dab264b1e9ea6570fd89f781ed9c98d5a0f1541
Caio Vinícius <caio1525pereira@gmail.com>
2023-06-22 21:42:24 -0300

    feat: :sparkles: Custom hook useMedia

---
+++ src/hooks/useMedia.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+/**
+ * @description Recebe uma media query como parâmetro e verifica se a medida atual da tela está de acordo com o parâmetro recebido
+ * @param media EX: "(max-width: 640px)"
+ * @returns Boolean
+ * @example useMedia("(max-width: 640px)") => largura atual da tela < 640px ? true : false
+ */
+
+export function useMedia(media: string) {
+  const [match, setMatch] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+
+    window.addEventListener("resize", changeMatch);
+
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [media]);
+
+  return match;
+}
