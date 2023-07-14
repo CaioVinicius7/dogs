84e790e7700072ecdaeb8a0e37dbaa0e29cbacab
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-14 00:06:28 -0300

    feat: :sparkles: Integração do formulário de criação de post com as bibliotecas zod e react hook form

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,7 @@
 import { ChangeEvent, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "../components/Form/Button";
 import { Input } from "../components/Form/Input";
@@ -13,10 +16,52 @@ interface Image {
   raw: File;
 }
 
+const createPostValidationSchema = z.object({
+  name: z
+    .string({
+      required_error: "Esse campo é obrigatório."
+    })
+    .min(3, {
+      message: "Esse campo deve ter no mínimo 3 caracteres."
+    }),
+  weight: z
+    .number({
+      required_error: "Esse campo é obrigatório."
+    })
+    .int({
+      message: "Esse campo deve ser um número inteiro."
+    })
+    .positive({
+      message: "Esse campo deve ter um valor positivo."
+    }),
+
+  age: z
+    .number({
+      required_error: "Esse campo é obrigatório."
+    })
+    .int({
+      message: "Esse campo deve ser um número inteiro."
+    })
+    .positive({
+      message: "Esse campo deve ter um valor positivo."
+    })
+});
+
+type CreatePostFormFields = z.infer<typeof createPostValidationSchema>;
+
 export function Post() {
   const [image, setImage] = useState<Image | null>(null);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreatePostFormFields>({
+    resolver: zodResolver(createPostValidationSchema),
+    shouldFocusError: true
+  });
 
   function handleImageChange({ target }: ChangeEvent<HTMLInputElement>) {
     if (!target.files) {
@@ -30,12 +75,38 @@ export function Post() {
     });
   }
 
+  function handleCreatePost({ name, weight, age }: CreatePostFormFields) {
+    console.log("Submit Realizado");
+    console.log({ name, weight, age });
+  }
+
   return (
     <section className={`${styles.photoPost} animationLeft`}>
-      <form className={styles.form}>
-        <Input name="Nome" label="Nome" type="text" />
-        <Input name="Peso" label="Peso" type="text" />
-        <Input name="Idade" label="Idade" type="text" />
+      <form className={styles.form} onSubmit={handleSubmit(handleCreatePost)}>
+        <Input
+          type="text"
+          label="Nome"
+          error={errors.name?.message}
+          {...register("name")}
+        />
+
+        <Input
+          type="number"
+          label="Peso"
+          error={errors.weight?.message}
+          {...register("weight", {
+            setValueAs: (v) => (v === "" ? undefined : parseInt(v, 10))
+          })}
+        />
+
+        <Input
+          type="number"
+          label="Idade"
+          error={errors.age?.message}
+          {...register("age", {
+            setValueAs: (v) => (v === "" ? undefined : parseInt(v, 10))
+          })}
+        />
 
         <div className={styles.uploadImage}>
           <Button
