c8364541808b5631ab14ab2cbd7c62f209c3e0bd
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-14 23:05:35 -0300

    feat: :sparkles: Integração com o endpoint de listagem de posts

--- src/services/post.ts
+++ src/services/post.ts
@@ -7,8 +7,26 @@ interface CreatePostRequest {
   img: File;
 }
 
+interface GetPostsRequest {
+  page: number;
+  itemsPerPage: number;
+  userId?: number;
+}
+
+interface GetPostsResponse {
+  id: number;
+  author: string;
+  title: string;
+  date: string;
+  src: string;
+  peso: string;
+  idade: string;
+  acessos: string;
+  total_comments: string;
+}
+
 export const postService = {
-  async createPost({ name, weight, age, img }: CreatePostRequest) {
+  createPost: async ({ name, weight, age, img }: CreatePostRequest) => {
     const formData = new FormData();
 
     formData.append("nome", name);
@@ -17,5 +35,30 @@ export const postService = {
     formData.append("img", img);
 
     await api.post("/api/photo", formData);
+  },
+  getPosts: async ({ page, itemsPerPage, userId = 0 }: GetPostsRequest) => {
+    const { data } = await api.get<GetPostsResponse[]>("/api/photo", {
+      params: {
+        _page: page,
+        _total: itemsPerPage,
+        _user: userId
+      }
+    });
+
+    const formattedData = data.map((photo) => {
+      return {
+        id: Number(photo.id),
+        author: photo.author,
+        title: photo.title,
+        date: photo.date,
+        url: photo.src,
+        weight: Number(photo.peso),
+        age: Number(photo.idade),
+        views: Number(photo.acessos),
+        comments: Number(photo.total_comments)
+      };
+    });
+
+    return formattedData;
   }
 };
