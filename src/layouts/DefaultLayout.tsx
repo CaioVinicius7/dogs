786c3c1598e03de6cd6572fd4ef917e01f3a3e28
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-16 20:42:21 -0300

    refactor: :lipstick: Estilização do defaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 
+import styles from "./DefaultLayout.module.css";
+
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
@@ -7,7 +9,11 @@ export function DefaultLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+
+      <div className={styles.defaultLayoutContentContainer}>
+        <Outlet />
+      </div>
+
       <Footer />
     </>
   );
