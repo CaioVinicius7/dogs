24395de0f578a578e64b9cac2402b31290f91662
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-26 21:05:42 -0300

    feat: :sparkles: Integração do formulário de recuperação de senha com as bibliotecas zod e react hook form

--- src/pages/PasswordLost.tsx
+++ src/pages/PasswordLost.tsx
@@ -1,15 +1,45 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import { Input } from "../components/Form/Input";
 import { Button } from "../components/Form/Button";
 
+const passwordLostFormValidationSchema = z.object({
+  email: z
+    .string({ required_error: "Esse campo é obrigatório." })
+    .min(3, "Esse campo deve ter no mínimo 3 caracteres.")
+});
+
+type PasswordLostFormFields = z.infer<typeof passwordLostFormValidationSchema>;
+
 export function PasswordLost() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<PasswordLostFormFields>({
+    resolver: zodResolver(passwordLostFormValidationSchema)
+  });
+
+  async function handleSendPasswordRecoveryEmail({
+    email
+  }: PasswordLostFormFields) {
+    console.log({ email });
+  }
+
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
 
-      <form>
-        <Input name="email" label="Email / Usuário" />
+      <form onSubmit={handleSubmit(handleSendPasswordRecoveryEmail)}>
+        <Input
+          label="Email / Usuário"
+          error={errors.email?.message}
+          {...register("email")}
+        />
 
-        <Button>Enviar Email</Button>
+        <Button isLoading={isSubmitting}>Enviar Email</Button>
       </form>
     </section>
   );
