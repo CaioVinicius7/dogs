1a065dea863fb8470533ee59daedff4cc7f895d9
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-29 19:58:08 -0300

    refactor: :children_crossing: Comportamento após excluir post

--- src/components/DeletePostButton.tsx
+++ src/components/DeletePostButton.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import styles from "./DeletePostButton.module.css";
 
-import { postService } from "../services/post";
 import { Spinner } from "./Spinner";
+
+import { postService } from "../services/post";
 
 interface DeletePostButtonProps {
   postId: number;
@@ -13,6 +15,9 @@ interface DeletePostButtonProps {
 export function DeletePostButton({ postId }: DeletePostButtonProps) {
   const [isLoading, setIsLoading] = useState(false);
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
   async function handleDeletePost() {
     try {
       setIsLoading(true);
@@ -20,6 +25,16 @@ export function DeletePostButton({ postId }: DeletePostButtonProps) {
       await postService.deletePost({
         postId: postId
       });
+
+      toast.success("Post excluído com sucesso.", {
+        theme: "colored"
+      });
+
+      if (location.pathname.includes("/post")) {
+        navigate(-1);
+
+        return;
+      }
 
       window.location.reload();
     } catch {
