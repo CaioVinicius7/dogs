da5994d3d1f6bb2b2c460054e0f1ac3b49d993e7
Caio Vinícius <caio1525pereira@gmail.com>
2023-07-29 20:03:33 -0300

    feat: :mag: Adição de tags de indexação na página Post

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import { Post as PostContent } from "../components/Post";
 
@@ -6,8 +7,16 @@ export function Post() {
   const { postId } = useParams();
 
   return (
-    <main className="container mt-2">
-      <PostContent postId={Number(postId)} single />
-    </main>
+    <>
+      <Helmet>
+        <title>Post | Dogs</title>
+
+        <meta name="robots" content="noindex" />
+      </Helmet>
+
+      <main className="container mt-2">
+        <PostContent postId={Number(postId)} single />
+      </main>
+    </>
   );
 }
